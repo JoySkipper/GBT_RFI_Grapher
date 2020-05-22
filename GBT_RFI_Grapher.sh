c63e7eeb3426e189c13ea7d81390507883921a4b
JoySkipper <jns9md@virginia.edu>
2020-05-21 23:10:15 -0400

    made compatible w python3 or 2

--- GBT_RFI_Grapher.sh
+++ GBT_RFI_Grapher.sh
@@ -1,8 +1,22 @@
 #which python3
 #python3 -m venv ./GBT_RFI_Grapher_env
-virtualenv -p /bin/python GBT_RFI_Grapher_env
-source ./GBT_RFI_Grapher_env/bin/activate
+python2=$(python --version | grep 'Python\ 2')
+python3=$(python --version | grep 'Python\ 3')
+#echo $python2
+#echo $python3
+if [[ Python\ 2 = $python2* ]];
+then
+    virtualenv -p python GBT_RFI_Grapher_env
+elif [[ Python\ 3 = $python3* ]];
+then   
+    python -m venv GBT_RFI_Grapher_env
+else
+    echo 'You need python installed to run this script.'
+    exit 1
+fi
+source GBT_RFI_Grapher_env/bin/activate
 pip install --upgrade pip
 pip install matplotlib
 python grapher.py $1
 deactivate
+rm -rf GBT_RFI_Grapher_env
